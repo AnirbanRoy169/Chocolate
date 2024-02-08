658bca27098b4ed59b5aead5a9288a0378d2b77d
Anirban Roy <156198466+AnirbanRoy169@users.noreply.github.com>
2024-02-08 19:42:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+var typed = new Typed('#lineOne', {
+    strings: ["Hey.."],
+    typeSpeed: 50,
+    showCursor: false,
+});
+var typed = new Typed('#lineTwo', {
+    strings: ["Janis to ajke Misti Day hole besi vlo hoto"],
+    startDelay: 1000,
+    typeSpeed: 50,
+    showCursor: false,
+});
+var typed = new Typed('#lineThree', {
+    strings: ["Kno bol to..."],
+    startDelay: 4700,
+    typeSpeed: 50,
+    showCursor: false,
+});
+var typed = new Typed('#lineFour', {
+    strings: ["Karon tumi to Misti"],
+    startDelay: 6000,
+    typeSpeed: 50,
+    showCursor: false,
+});
+var typed = new Typed('#lineFive', {
+    strings: ["Khub khub khub e misti 😋"],
+    startDelay: 7500,
+    typeSpeed: 50,
+    showCursor: false,
+});
+var typed = new Typed('#lineSix', {
+    strings: ["Iccha kore jno kheye ni akdom 🤤"],
+    startDelay: 10000,
+    typeSpeed: 50,
+    showCursor: false,
+});
+var typed = new Typed('#newLineOne', {
+    strings: ["Jai hok, kono bepar na"],
+    startDelay: 17000,
+    typeSpeed: 50,
+    showCursor: false,
+});
+var typed = new Typed('#newLineTwo', {
+    strings: ["Ak din to kheye e nibo 🌚"],
+    startDelay: 20000,
+    typeSpeed: 50,
+    showCursor: false,
+});
+var typed = new Typed('#newLineThree', {
+    strings: ["So ajke..."],
+    startDelay: 23000,
+    typeSpeed: 50,
+    showCursor: false,
+});
+var typed = new Typed('#newLineFour', {
+    strings: ["Happy Chocolate Day 🍫 to you"],
+    startDelay: 25000,
+    typeSpeed: 50,
+    showCursor: false,
+});
+var typed = new Typed('#newLineFive', {
+    strings: ["My Watashi No 😘"],
+    startDelay: 28000,
+    typeSpeed: 50,
+    showCursor: false,
+});
